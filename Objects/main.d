1cfe6246f601e2ff8f1919764334c3bbe85ee51d
Keshara <keshara2032@gmail.com>
2023-01-31 11:30:46 -0500

    Mini-Project-0 Deliverables

---
+++ Objects/main.d
@@ -0,0 +1,5 @@
+.\objects\main.o: Main.c
+.\objects\main.o: C:\Keil_v5\ARM\ARM_V5\Bin\..\include\stdio.h
+.\objects\main.o: C:\Keil_v5\ARM\ARM_V5\Bin\..\include\stdint.h
+.\objects\main.o: PLL.h
+.\objects\main.o: UART.h
---
+++ Objects/uart.d
@@ -0,0 +1,5 @@
+.\objects\uart.o: UART.c
+.\objects\uart.o: C:\Keil_v5\ARM\ARM_V5\Bin\..\include\stdio.h
+.\objects\uart.o: C:\Keil_v5\ARM\ARM_V5\Bin\..\include\stdint.h
+.\objects\uart.o: UART.h
+.\objects\uart.o: tm4c123gh6pm.h
